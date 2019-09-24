0ddeb84849389eaa8a03eb4bd63f3702bed2e5ad
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-09-24 18:05:10 -0500

    Project created.

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+      <View>
+        <Text>Texto</Text>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
