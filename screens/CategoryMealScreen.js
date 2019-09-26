6a4de91a772caf9aa8d86b1bdb3172428701ef38
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-09-25 20:09:59 -0500

    Added a small example for navigation.

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+
+import { CATEGORIES } from '../data/dummy-data'
 
 const CategoryMealScreen = props => {
+
+  const item = CATEGORIES.find( category => category.id === props.navigation.getParam('itemId') )
+
+  const goTo = () => {
+    props.navigation.navigate('MealDetail')
+  }
+
   return(
     <View style={styles.screen}>
-      <Text>Categories Screen</Text>
+      <Text>Categories Screen{item.title}</Text>
+      <Button title='Go to Details' onPress={goTo}/>
     </View>
   )
+}
+
+CategoryMealScreen.navigationOptions = ({navigation})=> {
+  const item = CATEGORIES.find( category => category.id === navigation.getParam('itemId') )
+
+  return {
+    headerTitle: item.title,   
+  }
 }
 
 const styles = StyleSheet.create({
